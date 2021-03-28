29b1db12437602f79193e1117c1651ccdc02df6e
WebBygger <sharifulpradhanhridoy@gmail.com>
2021-03-28 18:37:57 +0600

    server updated

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const admin = require("firebase-admin");
+const serviceAccount = require("service-account-file.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const uri = "mongodb+srv://arabian:burjAlArab7089@cluster0.zvgp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("burjAlArab").collection("bookings");
+  app.post("/addBooking", (req, res) => {
+    const newBooking = req.body;
+    collection.insertOne(newBooking)
+    .then(result =>{
+      res.send(result.insertedCount > 0);
+    })
+  })
+
+  app.get('/bookings', (req, res) => {
+    console.log(req.headers.authorization);
+    collection.find({email: req.query.email})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
+});
+
+
+
+
+
+
+
+app.listen(4200, () => console.log('listening to port 4200'))
